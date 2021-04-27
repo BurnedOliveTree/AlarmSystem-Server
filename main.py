b2db66de3d4cb0050cc427fcd0cfd55baba1527a
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2021-04-27 16:30:28 +0200

    added basic html capable of playing audio

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse, StreamingResponse
+from os import path
 
 app = FastAPI()
 
-@app.get('/')
-def root():
-    return {'msg': 'Hello world'}
+html = ''
+if path.isfile('index.html'):
+    with open('index.html') as file:
+        html = file.read()
+
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
+
+@app.get('/audio')
+def audio():
+    au = open('file.wav', mode='rb')
+    return StreamingResponse(au, media_type="audio/wav")
