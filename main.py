4646effd1a3ff9a77566df6ad66980d37cbde1d3
Paweł Muller <pawelmuller42@gmail.com>
2021-05-14 22:07:50 +0200

    Prepared server for Jinja2.

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-from fastapi import FastAPI, WebSocket
-from fastapi.responses import HTMLResponse, StreamingResponse
 from os import path
 
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, StreamingResponse
+from fastapi.templating import Jinja2Templates
+
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 html = ''
 if path.isfile('index.html'):
     with open('index.html') as file:
         html = file.read()
 
+
 @app.get("/")
 async def root():
     return HTMLResponse(html)
+
 
 @app.get('/audio')
 def audio():
     au = open('file.wav', mode='rb')
     return StreamingResponse(au, media_type="audio/wav")
+
+
+@app.get('/list_files')
+def list_files(request: Request):
+    files = ['a', 'b', 'c']
+
+    return templates.TemplateResponse(
+        "available_records.html.j2",
+        {"request": request, "files": files},
+    )
