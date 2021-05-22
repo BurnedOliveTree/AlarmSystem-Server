30c23a4f525aec12be926024b851807f4f6a8936
Paweł Muller <pawelmuller42@gmail.com>
2021-05-23 01:03:44 +0200

    Added settings server that sends new settings to attached devices.

--- routers/device.py
+++ routers/device.py
@@ -1,3 +1,5 @@
+import asyncio
+import json
 from shutil import copyfileobj
 
 from fastapi import APIRouter, status, File, UploadFile
@@ -7,6 +9,7 @@ from routers.database import db_report_alarm, db_add_record, db_update_device_se
 
 device = APIRouter()
 device.__name__ = "DeviceEndpoints"
+device.settings_broadcast = None
 
 RECORDS_PATH = "records"
 
@@ -29,5 +32,41 @@ async def report_alarm(device_id: int):
 @device.get("/update_settings")
 async def update_settings(device_id: int, name: str, is_armed: bool, recording_time: int):
     await db_update_device_settings(device_id, name, is_armed, recording_time)
-    return RedirectResponse(f"/settings?device_id={device_id}&name={name}&is_armed={is_armed}&recording_time={recording_time}",
-                            status_code=status.HTTP_303_SEE_OTHER)
+    device.settings_broadcast = {"device_id": device_id, "is_armed": is_armed, "recording_time": recording_time}
+    url = f"/settings?device_id={device_id}&name={name}&is_armed={is_armed}&recording_time={recording_time}"
+    return RedirectResponse(url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+@device.on_event("startup")
+async def startup_event():
+    device.task = asyncio.create_task(start_settings_server())
+    device.settings_broadcast = None
+
+
+async def send_settings():
+    while not device.settings_broadcast:
+        await asyncio.sleep(5)
+        continue
+    data = device.settings_broadcast
+    device.settings_broadcast = None
+    return data
+
+
+async def settings_sender(reader, writer):
+    data = await send_settings()
+    byte_data = json.dumps(data).encode('utf-8')
+
+    writer.write(byte_data)
+    await writer.drain()
+
+    print("Device received data")
+    writer.close()
+
+
+async def start_settings_server():
+    server = await asyncio.start_server(settings_sender, '0.0.0.0', 8888)
+    addr = server.sockets[0].getsockname()
+    print(f'Settings server running on {addr}')
+
+    async with server:
+        await asyncio.gather(server.serve_forever())
